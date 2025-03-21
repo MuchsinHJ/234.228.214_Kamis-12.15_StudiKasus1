db1e43e8189c042fc832ef4bc87b5075e1724936
MuchsinHJ <muchsinhj10@gmail.com>
2025-03-21 10:08:44 +0700

    menambah kode input dan output

--- tugas1.cpp
+++ tugas1.cpp
@@ -6,15 +6,32 @@ using namespace std;
 
 class Ktp {
   friend ostream &operator << (ostream &out, const Ktp &ktp);
-  friend istream &operator >> (istream &inp, const Ktp &ktp);
+  friend istream &operator >> (istream &inp,Ktp &ktp);
   public:
 
   private:
-  string nama;
+  string nik, prov, kab, nama, tempatTanggal,jenis, alamat, desa, camat, agama, status, pelerjaan;
+  int rt, rw;
+  char gol;
 };
-istream &operator >> (istream &inp, const Ktp &ktp){
-  
-}
-ostream &operator << (ostream &out, const Ktp &ktp){
+istream &operator>>(istream &inp,Ktp &ktp){
+  //Isi input dan output buat masukkin data
+  cout << "Masukkan NIK anda: ";
+  //ganti cin jadi inp
+  inp >> ktp.nik;
 
+  return inp;
+}
+ostream &operator<<(ostream &out, const Ktp &ktp){
+  //isi untuk keluaran outputnya atau tampilan ktp nya
+  // ganti cout jadi out
+  out << "NIK: " << ktp.nik;
+
+  return out;
+}
+
+int main(){
+  Ktp biodata;
+  cin >> biodata;
+  cout << biodata;
 }
